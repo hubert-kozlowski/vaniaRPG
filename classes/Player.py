e3f7ac327c6ed08595fe6fd21e2f77099b2b2bc4
blinno <business.kozlowskihubert@gmail.com>
2024-03-31 16:42:20 +0100

    Player attack/isalive

--- classes/Player.py
+++ classes/Player.py
@@ -1,3 +1,4 @@
+
 class Player:
     def __init__(self, name, health, damage, coins, inventory):
         self.name = name
@@ -5,3 +6,11 @@ class Player:
         self.damage = damage
         self.coins = coins
         self.inventory = inventory
+
+    def attack(self, enemy): # enemy is an instance of the Enemy class
+        print("You attacked the enemy!") 
+        enemy.health -= self.damage
+        print(f"Enemy health: {enemy.health}") 
+
+    def is_alive(self): # returns True if the player's health is greater than 0
+        return self.health > 0
